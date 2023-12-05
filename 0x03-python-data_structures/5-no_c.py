91343da318b91f225f72fe1f408d0c2e1b81e14c
Fatiiiimaaaa <oumhamedfatima0@gmail.com>
2023-12-04 16:27:12 -0800

    fix style for all files

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
@@ -6,4 +5,3 @@ def no_c(my_string):
         if elements != "c" and elements != "C":
             new_string += elements
     return new_string
-
